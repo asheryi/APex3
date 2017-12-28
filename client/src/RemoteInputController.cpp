f3bd1e4f3fcae03620ffd297c49292a404090f88
Avi Avra <avi19900@gmail.com>
2017-12-28 12:41:26 +0200

    Read Command Result

--- client/src/RemoteInputController.cpp
+++ client/src/RemoteInputController.cpp
@@ -2,7 +2,7 @@
 
 #include <zconf.h>
 #include "../include/RemoteInputController.h"
-
+#include <stdlib.h>
 RemoteInputController::RemoteInputController(int clientSocket) : clientSocket(clientSocket) {}
 
 Cell *RemoteInputController::getLandingPoint() const {
@@ -16,15 +16,17 @@ Cell *RemoteInputController::getLandingPoint() const {
 }
 string RemoteInputController::getRespond() const {
     // Read the move from the server
-
-    char* res;
-    int n = read(clientSocket, res, 20);
+    string d="df";
+    unsigned long sizeRespond;
+    int n = read(clientSocket, &sizeRespond, sizeof(unsigned long));
+    cout<<sizeRespond;
+    char* res=(char*) malloc(sizeof(char)*sizeRespond);
+     n = read(clientSocket, res, sizeRespond);
     if (n == -1) {
         throw "Error reading result from socket";
     }
-    string t=string(res);
-    cout<<t;
-    return t;
+    cout<<&res;
+    return d;
 }
 int RemoteInputController::getMenuSelection() const {
     return 0;
